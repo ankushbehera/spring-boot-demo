e0bb7e9deeaf223b86f165cc9e8e3927ac657984
ankushbehera <ankushkumar.behera@nagra.com>
2020-12-15 01:36:04 +0530

    checked in by script

--- commit.sh
+++ commit.sh
@@ -1,5 +1,10 @@
+message=$1
+if [ -z "$message" ]; then
+    message="checked in by script"
+fi
+
 git add .
-git commit -a -m "$1"
+git commit -a -m "$message"
 git push
 CONTAINER_ID=$(docker container ls -aqf "name=run-spring-boot-demo")
 winpty docker exec -it $CONTAINER_ID bash docker/docker_mvn.sh
