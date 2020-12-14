55a59b674ace0b95a1a6206fb669c918d613a86d
Behera Ankush kumar <ankushkumar.behera@nagra.com>
2020-12-15 00:12:43 +0530

    changes

--- src/main/java/com/ankush/springbootdemo/rest/ProductController.java
+++ src/main/java/com/ankush/springbootdemo/rest/ProductController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.*;
 
 @RestController
-@RequestMapping("/products")
+@RequestMapping("/product")
 public class ProductController {
     @Autowired
     private ProductService productService;
