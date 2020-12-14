b5b7e6db0de291dbaa8f697c53fdbc9ddb70b410
ankushbehera <ankushkumar.behera@nagra.com>
2020-12-15 03:23:29 +0530

    added by script

--- commit.sh
+++
@@ -1,9 +0,0 @@
-message=$1
-if [ -z "$message" ]; then
-    message="added by script"
-fi
-git add .
-git commit -a -m "$message"
-git push
-CONTAINER_ID=$(docker container ls -aqf "name=run-spring-boot-demo")
-winpty docker exec -it $CONTAINER_ID bash docker/docker_mvn.sh
