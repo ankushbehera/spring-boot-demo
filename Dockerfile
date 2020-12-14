335e3b5a2536ad4e2cc016214ee121e0886dd252
Behera Ankush kumar <ankushkumar.behera@nagra.com>
2020-12-15 00:10:57 +0530

    changes
    Signed-off-by: Behera Ankush kumar <ankushkumar.behera@nagra.com>

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM maven:3.6-jdk-11
-RUN ["sh","docker_mvn.sh"]
-WORKDIR /usr/src/spring-boot-demo
--- docker/docker_mvn.sh
+++ docker/docker_mvn.sh
@@ -1,5 +1,3 @@
-cd /usr/src
-git clone https://github.com/ankushbehera/spring-boot-demo.git
 cd /usr/src/spring-boot-demo
 git fetch origin
 git rebase origin
